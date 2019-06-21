d7fef3e1b74678da1ad0418ccb35bf50e3d41755
brynbarton <47806569+brynbarton@users.noreply.github.com>
2019-06-21 13:07:11 -0500

    T-Shirt section complete

--- interactive-form-v1/javascript/app.js
+++ interactive-form-v1/javascript/app.js
@@ -12,27 +12,27 @@ $design.find("option:eq(0)").hide();
 const $please = $("<option>Please select a T-Shirt Theme</option>").addClass(
   "defaultColor"
 );
+$color.find("option").hide();
+$color.prepend($please);
+$please.show();
+$color.find("option:eq(0)").attr("selected", "select");
 
 // Adding classes to Pun shirts
-$color.find("option:eq(0)").addClass("puns");
 $color.find("option:eq(1)").addClass("puns");
 $color.find("option:eq(2)").addClass("puns");
+$color.find("option:eq(3)").addClass("puns");
 
 // Adding classes to Heart shirts
-$color.find("option:eq(3)").addClass("heart");
 $color.find("option:eq(4)").addClass("heart");
 $color.find("option:eq(5)").addClass("heart");
-
-$(".puns").hide();
-$(".heart").hide();
-$color.prepend($please);
-$color.find("option:eq(0)").show();
+$color.find("option:eq(6)").addClass("heart");
 
 //Once T-Shirt theme is selected
 $design.change(function() {
   if ($design.val() === "js puns") {
     $color.find("option").hide();
     $(".puns").show();
+    $color.find("option:eq(1)").attr("selected", "puns");
   }
 });
 
@@ -40,5 +40,6 @@ $design.change(function() {
   if ($design.val() === "heart js") {
     $color.find("option").hide();
     $(".heart").show();
+    $color.find("option:eq(4)").attr("selected", "hearts");
   }
 });
